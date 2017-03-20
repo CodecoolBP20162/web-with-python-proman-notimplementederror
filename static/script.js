821e83ce6d69b06f2f8f965a5692cc8b05d9daa6
TahinDani <tahin.daniel@gmail.com>
2017-03-20 14:14:52 +0100

    drag n drop started

--- static/script.js
+++ static/script.js
@@ -14,6 +14,10 @@ $(document).ready(function(){
         console.log(retrievedObject.id);
         $('<div id="lole"></div>').appendTo("#lol");
 
+       $( function() {
+        $( "#sortable" ).sortable();
+        $( "#sortable" ).disableSelection();
+        } );
     });
 
 
@@ -27,6 +31,9 @@ $(document).ready(function(){
 
 });
 
+
+
+    
 
 
 
