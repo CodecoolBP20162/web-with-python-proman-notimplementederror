66ea3af3135b18123896e38cfd437f1de8f5ab8c
sador23 <adorjns@gmail.com>
2017-03-20 12:59:41 +0100

    added dynamic html generation

--- static/script.js
+++ static/script.js
@@ -1,3 +1,36 @@
-/**
- * Created by sador on 20.03.17.
- */
+$(document).ready(function(){
+    var count_id=0;
+    $('#sonka').click(function(){
+
+        //var text=document.getElementById('sonkisz').value;
+        var text=$('#sonkisz').val();
+        console.log(text);
+        var board = new Boards(text);
+
+        localStorage.setItem(board.id, JSON.stringify(board));
+        var retrievedObject = localStorage.getItem('text');
+        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        retrievedObject=JSON.parse(retrievedObject);
+        console.log(retrievedObject.id);
+        $('<div id="lole"></div>').appendTo("#lol");
+
+    });
+
+
+
+    function Boards(title){
+        this.id=count_id++;
+        this.title=title;
+        this.cards=[];
+
+    };
+
+});
+
+
+
+
+
+
+
+
