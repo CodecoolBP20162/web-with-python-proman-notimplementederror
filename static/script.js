eb613d51700529cc46438c72e949c4b4997102af
sador23 <adorjns@gmail.com>
2017-03-20 18:14:00 +0100

    work in progress

--- static/script.js
+++ static/script.js
@@ -1,32 +1,50 @@
 $(document).ready(function(){
-    var count_id=0;
 
+    var $article = document.getElementById('intro');
+
+    $('#intro').hide();
+    var count_id=0;
+    $('<div class="row">').appendTo("board_container");
     for(i in localStorage){
-        count_id++;
         var retrievedObject = localStorage.getItem(i);
         retrievedObject=JSON.parse(retrievedObject);
-        $('<div id="lole"><label></label></div>').text(retrievedObject.id).appendTo("#lol");
+            $('<div class="valami col-lg-4 col-md-4 col-sm-4 col-xs-4"><button></button></div>').text(retrievedObject.title).appendTo("body");
+        count_id++;
     }
 
+    $('</div>').appendTo("#board_container");
 
     $('#sonka').click(function(){
-
-        //var text=document.getElementById('sonkisz').value;
         var text=$('#sonkisz').val();
-        console.log(text);
         var board = new Boards(text);
         localStorage.setItem(board.id, JSON.stringify(board));
-        $('<div id="lole"><label></label></div>').text(board.id).appendTo("#lol");
-
+        $('<div class="valami col-lg-4 col-md-4 col-sm-4 col-xs-4" id='+ board.id+'></div>').text(board.title).appendTo("body");
     });
 
+    $('div').live("mouseenter",function(){
+        $(this).focus();
+    })
 
+    $('div').live('mouseenter', function(){
+        $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
+        })
+
+    $('div').live('mouseleave', function() {
+        $(this).animate({margin: 0, width: "-=10", height: "-=10"});
+    })
+
+    $('div').click(function(){
+       var id=$(this).id;
+       $('<div id="article">  <h1>'+$(this).id +'</h1> </div>').appendTo("body");
+       $('#intro').append()
+       $('#intro').toggle();
+
+    });
 
     function Boards(title){
         this.id=count_id++;
         this.title=title;
-        this.cards=[];
-
+        this.cards=["sonka","sali","lol"];
     };
 
 });
