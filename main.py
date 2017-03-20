8b4012ff2446d398fe12e867adab78ef1c88b6c0
sador23 <adorjns@gmail.com>
2017-03-20 10:50:02 +0100

    story 1

--- main.py
+++ main.py
@@ -3,28 +3,6 @@ from flask import Flask, request, g, redirect, url_for, render_template, session
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(
-    DEBUG=True, SECRET_KEY='any_random_string'))
-
-app.config.from_envvar('SUPER_SPRINTER_3000_SETTINGS', silent=True)
-
-def init_db():
-    identify = open("parameter.txt", "r")
-    login = identify.readlines()
-    identify.close()
-    db = PostgresqlDatabase(login[0], user=login[0])
-    try:
-        db.connect()
-        print("Database connection established.")
-    except:
-        print("Can't connect to database.\nPlease check your connection.txt file.")
-
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'postgre_db'):
-        g.postgre_db.close()
-
 
 @app.route('/')
 def home_menu():
@@ -32,5 +10,4 @@ def home_menu():
 
 
 if __name__ == "__main__":
-    init_db()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
