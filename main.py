1499c4a57254337600da588fa2a7384f0c2a4b02
TahinDani <tahin.daniel@gmail.com>
2017-03-21 10:44:21 +0100

    cards started

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app.config.from_object(__name__)
 def home_menu():
     return render_template('index.html')
 
-@app.route('/proba')
+@app.route('/cards')
 def proba():
-    return render_template('proba.html')
+    return render_template('cards.html')
 
 
 if __name__ == "__main__":
