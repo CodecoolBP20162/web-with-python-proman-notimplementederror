e475823514aa28454eb5b2655ddacc0c060d35ae
sador23 <adorjns@gmail.com>
2017-03-21 10:25:14 +0100

    boards qith jquery

--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     var $article = document.getElementById('intro');
+    var local_obj=[];
+    var click=true;
 
     $('#intro').hide();
     var count_id=0;
@@ -8,6 +10,8 @@ $(document).ready(function(){
     for(i in localStorage){
         var retrievedObject = localStorage.getItem(i);
         retrievedObject=JSON.parse(retrievedObject);
+        local_obj.push(retrievedObject);
+        console.log(retrievedObject);
             $('<div class="valami col-lg-4 col-md-4 col-sm-4 col-xs-4"><button></button></div>').text(retrievedObject.title).appendTo("body");
         count_id++;
     }
@@ -17,27 +21,25 @@ $(document).ready(function(){
     $('#sonka').click(function(){
         var text=$('#sonkisz').val();
         var board = new Boards(text);
+        local_obj.push(board);
         localStorage.setItem(board.id, JSON.stringify(board));
-        $('<div class="valami col-lg-4 col-md-4 col-sm-4 col-xs-4" id='+ board.id+'></div>').text(board.title).appendTo("body");
+        $('<div class="valami col-lg-4 col-md-4 col-sm-4 col-xs-4" id="'+ board.id+'"></div>').text(board.title).appendTo("body");
     });
 
-    $('div').live("mouseenter",function(){
-        $(this).focus();
-    })
-
-    $('div').live('mouseenter', function(){
-        $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
-        })
-
-    $('div').live('mouseleave', function() {
-        $(this).animate({margin: 0, width: "-=10", height: "-=10"});
-    })
-
-    $('div').click(function(){
-       var id=$(this).id;
-       $('<div id="article">  <h1>'+$(this).id +'</h1> </div>').appendTo("body");
-       $('#intro').append()
+    $('div').live('click',function(){
+       for(var i=0;i<local_obj.length;i++) {
+           if(local_obj[i].id==this.id) {
+               for(var j=0;j<local_obj[i].cards.length;j++){
+                $('<div id="new_div">  <h1>' + local_obj[i].cards[j] + '</h1> </div>').appendTo("#intro");
+               }
+           }
+       }
        $('#intro').toggle();
+       click=!click;
+       console.log(click);
+       if(!click){
+           $('#new_div').remove();
+       }
 
     });
 
