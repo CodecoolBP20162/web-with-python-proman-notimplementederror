05cc2540777cd03824e12c3ecc953724e4d5c3af
pengefutar <siri.lukacs@gmail.com>
2017-04-04 14:56:03 +0200

    renaming function names to make them more meaningful

--- static/script.js
+++ static/script.js
@@ -1,22 +1,15 @@
 $(document).ready(function () {
 
-        String.prototype.format = function () {
-        var args = [].slice.call(arguments);
-        return this.replace(/(\{\d+\})/g, function (a){
-            return args[+(a.substr(1,a.length-2))||0];
-        });
-        }
-
     var local_obj = [];
     var click = true;
     var count_board = 0;
     var count_task = 0;
     var count_id = 0;
     var board = new Boards('sample');
-    var task= new Tasks('sample');
+    var task = new Tasks('sample');
 
-
-    var factory = function (obj, title) {
+    // makes either a Board or a Task Card
+    var boardTaskFactory = function (obj, title) {
         if (obj == "card") {
             return new Tasks(title);
         }
@@ -28,7 +21,8 @@ $(document).ready(function () {
         }
     };
 
-    var status_finder = function (status, title) {
+    // sorts the tasks into their status group (new, in-progress, review, done)
+    var fillTaskListByStatus = function (status, title) {
         switch (status) {
             case "new":
                 $("#stat_new").append('<li id="li1" class="ui-state-default">' + title + '</li>');
@@ -60,26 +54,27 @@ $(document).ready(function () {
         }
     };
 
-    //At start, load all item from storage, and fill boards
+    // at start, load all item from storage, and fill boards
     var boardLoader = function () {
         board.get_cards();
     };
 
-    //When clicked on, adds a new boards with inputted text
-    var newBoardButton = function () {
+    // when clicked on, adds a new boards with inputted text
+    var saveNewBoardButton = function () {
         $('#newboard-button').click(function () {
             board.createBoard();
         });
     };
 
-    var toggleBoardInput = function () {
+    // opens and closes the new board pop-up when the plus button is clicked
+    var plusButton = function () {
         $('.add').click(function () {
             $('#input_fields').toggle();
         });
     };
 
-    //When clicked on, cards slide down, everything gets blurry
-    var clickBlurry = function () {
+    // fills up the task table pop up with the correct title, input, tasks, etc.
+    var taskPopUpFiller = function () {
         $('[id^="bc"]').live('click', function () {
             obj_id = this.id.toString();
             obj_id = obj_id.substr(2);
@@ -92,7 +87,7 @@ $(document).ready(function () {
                         $('#taskTitle').empty();
                         $('#taskTitle').append($('<h1>' + local_obj[i].title + '</h1><button id="card_add" class="' + obj_id + '">Create Card!</button><input type="text" id="card_text" placeholder="Task title" >'));
                         for (var j = 0; j < local_obj[i].cards.length; j++) {
-                            status_finder(local_obj[i].cards[j].status, local_obj[i].cards[j].title)
+                            fillTaskListByStatus(local_obj[i].cards[j].status, local_obj[i].cards[j].title)
                         }
 
                     } else {
@@ -104,37 +99,41 @@ $(document).ready(function () {
         });
     };
 
-    var newCardButton = function () {
+    // when the create card button is clicked in the task pop-up
+    // a new task is created and pushed into the local storage
+    var createNewTaskButton = function () {
         $('#card_add').live('click', function () {
             var card = $('#card_text').val();
-            status_finder("new", card);
+            fillTaskListByStatus("new", card);
             var retrievedObject = task.get_card(this.getAttribute('class'));
-            var task = factory("card", card);
+            var task = boardTaskFactory("card", card);
             retrievedObject = JSON.parse(retrievedObject);
             retrievedObject.cards.push(task);
             task.save_card(retrievedObject)
         });
     };
 
-    var divClicked = function () {
+    // when a task is clicked, an editable textarea gets created
+    var createTextareaInsteadListItem = function () {
         var text = $(this).text();
         var length = text.length;
         var editableText = $("<textarea />");
         editableText.val(text);
         $(this).replaceWith(editableText);
         editableText.focus();
-        editableText.blur(editableTextBlurred);
+        editableText.blur(saveTextareaValueIntoListItem);
     };
 
-    var editableTextBlurred = function () {
+    // saves textarea value into the html
+    var saveTextareaValueIntoListItem = function () {
         var html = $(this).val();
         var viewableText = $("<li id='li1'>");
         viewableText.html(html);
         $(this).replaceWith(viewableText);
-        $(viewableText).click(divClicked);
+        $(viewableText).click(createTextareaInsteadListItem);
     };
 
-    var taskTableToggle = function () {
+    var showHideTaskTable = function () {
         $(document).click(function () {
             if (click) {
                 if (this.id != '#task_table') {
@@ -154,90 +153,86 @@ $(document).ready(function () {
         this.state = new LocalStorage();
         this.get_cards = function () {
             this.state.get_cards()
-        }
+        };
 
         this.createBoard = function () {
             this.state.createBoard();
         }
+    }
 
-
-    };
-
-        function Tasks(title) {
+    function Tasks(title) {
         count_task++;
         task_id = 'task{0}'.format(count_task);
         this.id = task_id;
         this.title = title;
         this.status = "new";
         this.state = new LocalStorage();
-        this.get_card = function(){
+        this.get_card = function () {
             this.state.get_card()
-        }
-        this.save_card=function(){
+        };
+        this.save_card = function () {
             this.state.save_card()
         }
-    };
+    }
 
     function LocalStorage() {
         this.createBoard = function () {
             count_board++;
             var text = $('#newboard-input').val();
-            var board = factory("board", text);
+            var board = boardTaskFactory("board", text);
             local_obj.push(board);
             console.log(board.id);
             localStorage.setItem(board.id, JSON.stringify(board));
             strng = '<div class="boards col-lg-4 col-md-4 col-sm-4 col-xs-4" id="bc' + retrievedObject.id + '">' +
-                        '<p>' +
-                        '<img src="https://c1.staticflickr.com/1/674/20942077784_5d3ffb2ed0_h.jpg" />' + '</p>' +
-                        '<p id=title>Title</p>' +
-                    '</div>';
+                '<p>' +
+                '<img src="https://c1.staticflickr.com/1/674/20942077784_5d3ffb2ed0_h.jpg" />' + '</p>' +
+                '<p id=title>Title</p>' +
+                '</div>';
 
             $(strng).text(board.title).appendTo(".row");
-        }
+        };
         this.get_cards = function () {
             for (i in localStorage) {
                 var retrievedObject = localStorage.getItem(i);
                 retrievedObject = JSON.parse(retrievedObject);
                 local_obj.push(retrievedObject);
                 strng = '<div class="boards col-lg-4 col-md-4 col-sm-4 col-xs-4" id="bc' + retrievedObject.id + '">' +
-                            '<p id="image_">' +
-                            '<img src="https://c1.staticflickr.com/1/674/20942077784_5d3ffb2ed0_h.jpg" /></p>' +
-                            '<p id=title>' + retrievedObject.title + '</p>' +
+                    '<p id="image_">' +
+                    '<img src="https://c1.staticflickr.com/1/674/20942077784_5d3ffb2ed0_h.jpg" /></p>' +
+                    '<p id=title>' + retrievedObject.title + '</p>' +
                     '   </div>';
                 $(strng).appendTo(".row");
                 count_id++;
             }
         };
 
-        this.get_card=function (id) {
+        this.get_card = function (id) {
             return localStorage.getItem(id)
-        }
-        this.save_card=function (retrievedObject) {
+        };
+        this.save_card = function (retrievedObject) {
             localStorage.setItem(retrievedObject.id, JSON.stringify(retrievedObject));
 
         }
     }
 
 
-
     //-----for the sortable cards-------
-    $( "#stat_new, #stat_inprogress, #stat_review, #stat_done"  ).sortable({
-      connectWith: ".connectedSortable"
-      }).disableSelection();
+    $("#stat_new, #stat_inprogress, #stat_review, #stat_done").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
 
 
     var main = function () {
         $('#task_table').hide();
         stringFormat();
         boardLoader();
-        newBoardButton();
-        toggleBoardInput();
-        clickBlurry();
-        newCardButton();
-        $('#li1').live('click', divClicked);
-        taskTableToggle();
+        saveNewBoardButton();
+        plusButton();
+        taskPopUpFiller();
+        createNewTaskButton();
+        $('#li1').live('click', createTextareaInsteadListItem);
+        showHideTaskTable();
     };
-
 
 
     main();
