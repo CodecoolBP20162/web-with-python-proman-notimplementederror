91d5f32d4458cc31bf1298474bccd95d0aeae53e
sador23 <adorjns@gmail.com>
2017-04-06 09:10:23 +0200

    urlparse added

--- conn.py
+++ conn.py
@@ -24,9 +24,9 @@ class Tasks(BaseModel):
 
 
 if 'HEROKU' in os.environ:
-    import urlparse, psycopg2
-    urlparse.uses_netloc.append('postgres')
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    import urllib.parse, psycopg2
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
     db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
     db_proxy.initialize(db)
 else:
